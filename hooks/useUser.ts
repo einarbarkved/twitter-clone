6b351071df8c329563a8d14f923cab2e3d9fe93a
einarbarkved <einar.barkved@gmail.com>
2023-07-01 23:21:07 +0200

    Stuff

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -3,7 +3,7 @@ import useSWR from "swr";
 import fetcher from "@/libs/fetcher";
 
 const useUsers = (userId: number) => {
-  const { data, error, isLoading, mutate } = useSWR(userId ? `/api/users${userId}` : null, fetcher);
+  const { data, error, isLoading, mutate } = useSWR(userId ? `/api/users/${userId}` : null, fetcher);
 
   return {
     data,
---
+++ pages/users/[userId].tsx
@@ -0,0 +1,30 @@
+import Header from "@/components/Header";
+import useUser from "@/hooks/useUser";
+import { useRouter } from "next/router";
+import React from "react";
+import { ClipLoader } from "react-spinners";
+
+const UserView = () => {
+  const router = useRouter();
+  const { userId } = router.query;
+
+  const { data: fetchedUser, isLoading } = useUser(Number(userId) as number);
+
+  console.log(userId);
+
+  if (isLoading || !fetchedUser) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <ClipLoader color="lightblue" size={80} />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Header showBackArrow label="User Profile" />
+    </>
+  );
+};
+
+export default UserView;
